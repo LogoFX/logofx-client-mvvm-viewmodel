979b3928e6b912c16874ba63bf275e159e471f80
Gennady Verdel <godrose@gmail.com>
2019-05-15 15:37:52 +0300

    feat(concurrency-dict): added concurrency test to reproduce the issue #6

---
+++ src/LogoFX.Client.Mvvm.ViewModel.Tests/WrappingCollectionTests/ConcurrencyTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading;
+using FluentAssertions;
+using Xunit;
+
+namespace LogoFX.Client.Mvvm.ViewModel.Tests.WrappingCollectionTests
+{
+    public class ConcurrencyTests
+    {
+        static ConcurrencyTests()
+        {
+            Dispatch.Current = new DefaultDispatch();
+        }
+
+        [Fact]
+        public void Add_ModelIsAddedAndThenRemovedAndThenReAdded_ExceptionIsNotThrown()
+        {
+            var lastModel = new TestModel(3);
+            var dataSource =
+                new ObservableCollection<TestModel>(new[] {new TestModel(1), new TestModel(2)});
+
+            var wc = new WrappingCollection(r => r.UseConcurrent()).WithSource(dataSource);
+            dataSource.Add(lastModel);
+            dataSource.Remove(lastModel);
+            var exception = Record.Exception(() => dataSource.Add(lastModel));
+
+            exception.Should().BeNull();
+        }
+    }
+}
