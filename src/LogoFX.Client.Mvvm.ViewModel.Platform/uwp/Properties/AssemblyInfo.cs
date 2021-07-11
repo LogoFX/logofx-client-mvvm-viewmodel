3d318b277d62cc09611e0bc914b95fe3c2ca438c
Gennady Verdel <godrose@gmail.com>
2021-07-11 18:07:37 +0300

    devops: bump version

--- src/LogoFX.Client.Mvvm.ViewModel.Platform/uwp/Properties/AssemblyInfo.cs
+++ src/LogoFX.Client.Mvvm.ViewModel.Platform/uwp/Properties/AssemblyInfo.cs
@@ -21,7 +21,7 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:[assembly: AssemblyVersion("2.2.0")
-[assembly: AssemblyVersion("2.2.0")]
-[assembly: AssemblyFileVersion("2.2.0")]
+// by using the '*' as shown below:[assembly: AssemblyVersion("2.2.1-rc1")
+[assembly: AssemblyVersion("2.2.1")]
+[assembly: AssemblyFileVersion("2.2.1")]
 [assembly: ComVisible(false)]
